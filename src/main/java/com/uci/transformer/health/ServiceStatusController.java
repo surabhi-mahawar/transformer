d299b86274a2b96c368025a1be05841ead7ad98a
Surabhi <surabhi.mahawar@aurigait.com>
2021-08-04 23:12:49 +0530

    health apis

--- src/main/java/com/uci/transformer/health/ServiceStatusController.java
+++ src/main/java/com/uci/transformer/health/ServiceStatusController.java
@@ -1,9 +1,17 @@
 package com.uci.transformer.health;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.uci.utils.UtilHealthService;
+
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/service")
 public class ServiceStatusController {
+	
+	@Autowired
+	private UtilHealthService healthService;
 
-    @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":true}}");
-        return ResponseEntity.ok(json);
+	@RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
+    public ResponseEntity<JsonNode> kafkaStatusCheck() throws IOException, JsonProcessingException {
+    	JsonNode jsonNode = getResponseJsonNode();
+    	((ObjectNode) jsonNode).put("result", healthService.getKafkaHealthNode());
+        
+        return ResponseEntity.ok(jsonNode);
+    }
+    
+    @RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json", "text/json" })
+    public ResponseEntity<JsonNode> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
+    	JsonNode jsonNode = getResponseJsonNode();
+        ((ObjectNode) jsonNode).put("result", healthService.getCampaignUrlHealthNode());
+        
+        return ResponseEntity.ok(jsonNode);
+    }
+    
+    /**
+     * Returns json node for service response
+     * 
+     * @return JsonNode
+     * @throws JsonMappingException
+     * @throws JsonProcessingException
+     */
+    private JsonNode getResponseJsonNode() throws JsonMappingException, JsonProcessingException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
+        return jsonNode;
     }
 }
